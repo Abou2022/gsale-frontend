48d3fc38bcca9330af084a3d53b158198b63eba8
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-06-07 07:32:42 -0700

    markers display and map is centered around them

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,71 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Leaflet from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import './map.css';
-
-Leaflet.Icon.Default.imagePath = '../node_modules/leaflet';
-
-delete Leaflet.Icon.Default.prototype._getIconUrl;
-
-Leaflet.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});
-
-// import {  } from '../../actions/userAuth-actions.js';
+import { vendorsFetchRequest } from '../../actions/vendors-actions';
+import { logError, } from '../../lib/util';
+import MapLeaflet from '../mapLeaflet';
 
 class Map extends Component {
     constructor(props) {
         super(props);
         this.state = {};
-        // this.handleStatusChange = this.handleStatusChange.bind(this);
     }
 
     componentDidMount() {
-        console.log(this.props.userAuth);
-    };
-
-    componentDidUpdate(prevProps, prevState) {
-        // if (prevState.count !== this.state.count) {
-        //     document.title = `You clicked ${this.state.count} times`;
-        // }
+        this.props.vendorsFetch()
+            .catch(err => logError(err));
     }
 
-    componentWillUnmount() {
-
-    };
-
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.vendors !== this.props.vendors
+    }
     render() {
         return (
-            <div id="map">
-                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={[51.505, -0.09]}>
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </Marker>
-                </MapContainer>
-            </div>
-        );
+            <MapLeaflet coords={this.props.vendors} />
+        )
     }
 }
 
 const mapStateToProps = state => ({
-    userAuth: state.userAuth,
-    userProfile: state.userProfile,
-    attendees: state.attendess,
-    comments: state.comments,
-    garageSaleEvent: state.garageSaleEvent,
     vendors: state.vendors
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => {
+    return {
+        vendorsFetch: () => dispatch(vendorsFetchRequest()),
+    };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Map);
+
+    // componentDidMount() {
+    //     if ("geolocation" in navigator) {
+    //         navigator.geolocation.getCurrentPosition(
+    //             function (position) {
+    //                 getZipsArray(position.coords.latitude, position.coords.longitude);
+    //                 console.log("position: ", position);
+    //                 console.log("Latitude is :", position.coords.latitude);
+    //                 console.log("Longitude is :", position.coords.longitude);
+    //             },
+    //             function (error) {
+    //                 console.error("Error Code = " + error.code + " - " + error.message);
+    //             }
+    //         );
+    //     } else {
+    //         console.log("no navigator in location");
+    //     }
+    // }
