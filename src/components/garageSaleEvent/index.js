05857deb59f268d6f05e0163b01225f2574c6c40
Bakary Sylla <syllabakary2002@gmail.com>
2022-06-08 14:00:44 -0700

    check if is work

--- src/components/garageSaleEvent/index.js
+++ src/components/garageSaleEvent/index.js
@@ -67,10 +67,10 @@ function GarageSaleEvent() {
         </div>
 
         {/* upload images files */}
-        <div className="input-group mb-3">
+        {/* <div className="input-group mb-3">
           <input type="file" className="form-control" id="inputGroupFile02" />
           <label className="input-group-text" htmlFor="inputGroupFile02">Upload</label>
-        </div>
+        </div> */}
 
       </div>
 
